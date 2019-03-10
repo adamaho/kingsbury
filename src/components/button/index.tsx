f55f734089a9b52650c429d25c6882ee59d2f689
aaho <aaho@clearpath.ai>
2019-03-10 09:19:30 -0400

    adding the rest of the button styles

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -24,9 +24,30 @@ const ButtonComponent: React.FunctionComponent<IButtonProps> = (props) => {
 }
 
 const Button = styled(ButtonComponent)`
-  background-color: red;
+  background: ${(props: IButtonProps) => props.theme.buttons[props.type].background};
+  color: ${(props: IButtonProps) => props.theme.buttons[props.type].color};
+  height: ${(props: IButtonProps) => `${props.theme.buttons.default.height}px`};
 
-  padding: 100px;
+  font-family: ${(props: IButtonProps) => props.theme.fontFamily};
+  font-weight: 700;
+  font-size: 16px;
+  letter-spacing: 2px;
+  text-decoration: none;
+
+  border: ${(props: IButtonProps) => props.theme.buttons[props.type].border};
+  border-radius: 4px;
+
+  padding: 5px 0px 0px 0px;
+  box-shadow: ${(props: IButtonProps) => props.theme.buttons[props.type].boxShadow};
+
+  cursor: pointer;
+
+  transition: border-radius 0.25s;
+
+  &:hover {
+    border-radius: ${(props: IButtonProps) => `${props.theme.buttons.default.height / 2}px`};
+  }
+
   &:active {
     opacity: 0.7;
   }
