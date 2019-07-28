600018511a42946127566eb0f76bfa3032b1fb76
aaho <aaho@clearpath.ai>
2019-07-28 17:39:38 -0400

    removing margin from typeography components

--- src/components/typeography/index.tsx
+++ src/components/typeography/index.tsx
@@ -6,22 +6,12 @@ export interface ITypeographyProps {
   theme: ITheme;
 }
 
-export const H1 = styled.h1`
-  margin: ${(props: ITypeographyProps) => props.theme.typeography.h1.margin};
-`;
+export const H1 = styled.h1``;
 
-export const H2 = styled.h2`
-  margin: ${(props: ITypeographyProps) => props.theme.typeography.h2.margin};
-`;
+export const H2 = styled.h2``;
 
-export const H3 = styled.h3`
-  margin: ${(props: ITypeographyProps) => props.theme.typeography.h3.margin};
-`;
+export const H3 = styled.h3``;
 
-export const H4 = styled.h4`
-  margin: ${(props: ITypeographyProps) => props.theme.typeography.h4.margin};
-`;
+export const H4 = styled.h4``;
 
-export const P = styled.p`
-  margin: ${(props: ITypeographyProps) => props.theme.typeography.p.margin};
-`;
+export const P = styled.p``;
