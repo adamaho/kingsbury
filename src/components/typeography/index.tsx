b1db08fb910da73027a35314814b1c18b72a610a
aaho <aaho@clearpath.ai>
2019-08-02 14:50:03 -0400

    changing to divs

--- src/components/typeography/index.tsx
+++ src/components/typeography/index.tsx
@@ -7,21 +7,26 @@ export interface ITypeographyProps {
 }
 
 export const H1 = styled.span`
+  display: block;
   font-size: 32px;
 `;
 
 export const H2 = styled.span`
+  display: block;
   font-size: 24px;
 `;
 
 export const H3 = styled.span`
+  display: block;
   font-size: 18px
 `;
 
 export const H4 = styled.span`
+  display: block;
   font-size: 16px;
 `;
 
 export const P = styled.span`
+  display: block;
   font-size: 12px;
 `;
