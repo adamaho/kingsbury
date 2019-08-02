5187d8cab836dfe62f35480ee85c684c971be51b
aaho <aaho@clearpath.ai>
2019-08-02 14:50:36 -0400

    changing to fivs

--- src/components/typeography/index.tsx
+++ src/components/typeography/index.tsx
@@ -6,27 +6,22 @@ export interface ITypeographyProps {
   theme: ITheme;
 }
 
-export const H1 = styled.span`
-  display: block;
+export const H1 = styled.div`
   font-size: 32px;
 `;
 
-export const H2 = styled.span`
-  display: block;
+export const H2 = styled.div`
   font-size: 24px;
 `;
 
-export const H3 = styled.span`
-  display: block;
+export const H3 = styled.div`
   font-size: 18px
 `;
 
-export const H4 = styled.span`
-  display: block;
+export const H4 = styled.div`
   font-size: 16px;
 `;
 
-export const P = styled.span`
-  display: block;
+export const P = styled.div`
   font-size: 12px;
 `;
