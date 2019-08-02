3564f58424a1a51f89b2f389676fe0c11b8f7fb6
aaho <aaho@clearpath.ai>
2019-08-01 21:56:50 -0400

    export tag

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ import Button from './button';
 import Card from './card';
 import Input from './input';
 import Notice from './notice';
+import Tag from './tag';
 
 import {
   H1,
@@ -16,6 +17,7 @@ export {
   Card,
   Input,
   Notice,
+  Tag,
   H1,
   H2,
   H3,
